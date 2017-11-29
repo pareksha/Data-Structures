d67629d55aedac8e17c96df9b8adf400ecd2eb85
pareksha <pareksha.manchanda@gmail.com>
2017-11-29 18:02:46 +0530

    Linked list with nodes in ascending order

--- linked_list.c
+++ linked_list.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 struct node{
   int data;
   struct node* link;
@@ -17,29 +18,59 @@ void display(){
   printf("\n");
 }
 
-void insertion(struct node* cur,struct node* pre){
+void insertion(struct node* target_pointer){
+  bool flag;
+  struct node* pre;
+  struct node* cur;
   printf("\nEnter value: ");
-  scanf("%d",&(cur -> data));
+  scanf("%d",&(target_pointer -> data));
   if (START == NULL){
-    START = cur;
-  }else{
-  pre ->link = cur;
+    START = target_pointer;
+    target_pointer -> link = NULL;
   }
-  cur -> link = NULL;
+  else{
+    flag = false;
+    pre = NULL;
+    cur = START;
+    while(cur != NULL){
+      if(target_pointer -> data == cur -> data){
+        flag = true;
+        break;
+      }
+      else if(target_pointer -> data > cur -> data){
+      pre = cur;
+      cur = cur -> link;
+      }
+      else
+      break;
+    }
+    if (flag == true){
+      printf("%d is already present in the list\n",target_pointer -> data);
+    }
+    else{
+      if(pre == NULL){
+      START = target_pointer;
+      target_pointer -> link = cur;
+      }
+      else{
+      pre -> link = target_pointer;
+      target_pointer -> link = cur;
+      }
+    }
+  }
 }
 
 void main(){
   struct node n1,n2,n3,n4,n5;
   START = NULL;
-  insertion(&n1,NULL);
-  // printf("%p",START);
+  insertion(&n1);
   display();
-  insertion(&n2,&n1);
+  insertion(&n2);
   display();
-  insertion(&n3,&n2);
+  insertion(&n3);
   display();
-  insertion(&n4,&n3);
+  insertion(&n4);
   display();
-  insertion(&n5,&n4);
+  insertion(&n5);
   display();
 }
