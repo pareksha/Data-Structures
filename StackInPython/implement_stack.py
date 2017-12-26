c7122bc5496a88dcca396dc92513c924c3fa427e
pareksha <pareksha.manchanda@gmail.com>
2017-12-26 21:44:24 +0530

    Add implementation using linked list

--- StackInPython/implement_stack.py
+++ StackInPython/implement_stack.py
@@ -1,4 +1,4 @@
-from stack import Stack
+from stack import *
 
 stack_1 = Stack()
 
@@ -27,3 +27,23 @@ stack_1.pop()
 print(stack_1)
 
 print('-'*15)
+
+stack_2 = LinkedStack()
+
+print(stack_2.is_empty())
+
+print('-'*15)
+
+stack_2.push(20)
+stack_2.push(12)
+stack_2.push(145)
+stack_2.print_stack()
+
+print('-'*15)
+
+stack_2.pop()
+stack_2.print_stack()
+
+print('-'*15)
+
+print(stack_2.is_empty())
--- StackInPython/stack.py
+++ StackInPython/stack.py
@@ -1,3 +1,4 @@
+# Implement Stack using array
 class Stack:
     def __init__(self):
         self.stack = list()
@@ -19,3 +20,62 @@ class Stack:
 
     def __str__(self):
         return str(self.stack)
+
+
+class Node:
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    def set_next(self, next=None):
+        self.next_node = next
+
+    def get_next(self):
+        return self.next_node
+
+    def get_data(self):
+        return self.data
+
+
+# Implement stack using linked list
+class LinkedStack:
+    def __init__(self):
+        self.stack = None
+
+    def push(self, data):
+        new_node = Node(data)
+
+        if self.stack is None:
+            self.stack = new_node
+        else:
+            walker = self.stack
+            while walker.get_next() is not None:
+                walker = walker.get_next()
+
+            walker.set_next(new_node)
+
+    def pop(self):
+        if self.stack is None:
+            print('Stack is empty')
+            return
+        pre_walker = None
+        walker = self.stack
+        while walker.get_next() is not None:
+            pre_walker = walker
+            walker = walker.get_next()
+        pre_walker.set_next(None)
+
+    def is_empty(self):
+        if self.stack is None:
+            return True
+        else:
+            return False
+
+    def print_stack(self):
+        if self.stack is None:
+            print('Stack is empty')
+            return
+        walker = self.stack
+        while walker is not None:
+            print(walker.get_data())
+            walker = walker.get_next()
