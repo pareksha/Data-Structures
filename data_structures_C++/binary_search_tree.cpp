39a32f3fd6c550d32d7b2a0106f089cd80d46954
pareksha <pareksha.manchanda@gmail.com>
2018-10-29 13:43:55 +0530

    Add corrections to BST (for delete)

--- data_structures_C++/binary_search_tree.cpp
+++ data_structures_C++/binary_search_tree.cpp
@@ -19,28 +19,59 @@ class BST {
             displayHelper(root_ -> left, spaces);
         }
         void preorderTraversalHelper(struct node* root_) {
-            if(!root_) return;
+            if(root_ == NULL) return;
             cout << root_ -> data << "\t";
             preorderTraversalHelper(root_ -> left);
-            preorderTraversalHelper(root -> right);
+            preorderTraversalHelper(root_ -> right);
         }
         void postorderTraversalHelper(struct node* root_) {
-            if(!root_) return;
+            if(root_ == NULL) return;
             postorderTraversalHelper(root_ -> left);
-            postorderTraversalHelper(root -> right);
+            postorderTraversalHelper(root_ -> right);
             cout << root_ -> data << "\t";
         }
         void inorderTraversalHelper(struct node* root_) {
-            if(!root_) return;
+            if(root_ == NULL) return;
             inorderTraversalHelper(root_ -> left);
             cout << root_ -> data << "\t";
-            inorderTraversalHelper(root -> right);
+            inorderTraversalHelper(root_ -> right);
         }
-        int flag;
+        void searchHelper(int value, struct node* &parent, struct node* &ptr) {
+            for(int i = 0; i < 7; i++) {
+                if(ptr == NULL) {
+                    cout << value << " is NOT present in BST." << endl;
+                    ptr = NULL;
+                    parent = NULL;
+                    return;
+                } else if(value < ptr -> data) {
+                    if(ptr -> left == NULL) {
+                        cout << value << " is NOT present in BST." << endl;
+                        ptr = NULL;
+                        parent = NULL;
+                        return;
+                    } else {
+                        parent = ptr;
+                        ptr = ptr -> left;
+                    }
+                } else if(value > ptr -> data) {
+                    if(ptr -> right == NULL) {
+                        cout << value << " is NOT present in BST." << endl;
+                        ptr = NULL;
+                        parent = NULL;
+                        return;
+                    } else {
+                        parent = ptr;
+                        ptr = ptr -> right;
+                    }
+                } else {
+                    cout << value << " is present in BST." << endl;
+                    return;
+                }
+            }
+        }
     public:
         BST() {
             root = NULL;
-            flag = 0;
         }
         void initialise() {
             int arr[7];
@@ -85,161 +116,63 @@ class BST {
             displayHelper(root, 0);
         }
         void preorderTraversal() {
+            cout << "Preorder Traversal is \t";
             preorderTraversalHelper(root);
+            cout << endl;
         }
         void inorderTraversal() {
+            cout << "Inorder Traversal is \t";
             inorderTraversalHelper(root);
+            cout << endl;
         }
         void postorderTraversal() {
+            cout << "Postorder Traversal is \t";
             postorderTraversalHelper(root);
+            cout << endl;
         }
-        struct node* search(int value) {
+
+        void search(int value) {
+            cout << "Searching for " << value << endl;
             struct node* parent = NULL;
             struct node* ptr = root;
-            for(int i = 0; i < 7; i++) {
-                if(ptr == NULL) {
-                    cout << value << " is NOT present in BST." << endl;
-                    return NULL;
-                } else if(value < ptr -> data) {
-                    if(ptr -> left == NULL) {
-                        cout << value << " is NOT present in BST." << endl;
-                        return NULL;
-                    } else {
-                        parent = ptr;
-                        ptr = ptr -> left;
-                    }
-                } else if(value > ptr -> data) {
-                    if(ptr -> right == NULL) {
-                        cout << value << " is NOT present in BST." << endl;
-                        return NULL;
-                    } else {
-                        parent = ptr;
-                        ptr = ptr -> right;
-                    }
-                } else {
-                    cout << value << " is present in BST." << endl;
-                    if(root == ptr) flag = 1;
-                    return parent;
-                }
-            }
+            searchHelper(value, parent, ptr);
         }
         
         void delete_(int value) {
-            struct node* parent = search(value);
-            if(parent == NULL) {
-                if(flag = 0) return;
+            struct node* parent = NULL;
+            struct node* ptr = root;
+            searchHelper(value, parent, ptr);
+            if(parent == NULL && ptr == NULL) return;
+            cout << "Deleting value " << value << endl;
+            if(ptr -> left == NULL && ptr -> right == NULL) {
+                if(ptr == parent -> left) parent -> left = NULL;
+                else if(ptr == parent -> right) parent -> right = NULL;
+                delete ptr;
+            } else if(ptr -> left != NULL && ptr -> right == NULL) {
+                if(ptr == parent -> left) parent -> left = ptr -> left;
+                else if(ptr == parent -> right) parent -> right = ptr -> left;
+                delete ptr;
+            } else if (ptr -> right != NULL && ptr -> left == NULL) {
+                if(ptr == parent -> left) parent -> left = ptr -> right;
+                else if(ptr == parent -> right) parent -> right = ptr -> right;
+                delete ptr;
+            } else if (ptr -> left != NULL && ptr -> right != NULL) {
+                struct node* max = ptr -> right;
+                struct node* max_parent = ptr;
+                if(max -> left == NULL) {
+                    ptr -> data = max -> data;
+                    ptr -> right = max -> right;
+                    delete max;
+                }
                 else {
-                    flag = 0;
-                    if(root -> left == NULL && root -> right == NULL) {
-                        struct node* temp = root;
-                        root = NULL;
-                        delete temp;
-                    } else if(root -> left != NULL && root -> right == NULL) {
-                        struct node* temp = root;
-                        root = root -> left;
-                        delete temp;
-                    } else if (root -> right != NULL && root -> left == NULL) {
-                        struct node* temp = root;
-                        root = root -> right;
-                        delete temp;
-                    } else if (root -> left != NULL && root -> right != NULL) {
-                        struct node* max = root -> right;
-                        struct node* max_parent = root;
-                        if(max -> left == NULL) {
-                            root -> data = max -> data;
-                            root -> right = max -> right;
-                            delete max;
-                        }
-                        else {
-                            while(max -> left != NULL) {
-                                max_parent = max;
-                                max = max -> left;
-                            }
-                            if(max -> left == NULL && max -> right == NULL) {
-                                max_parent -> left = NULL;
-                            } else if(max -> left != NULL && max -> right == NULL) {
-                                max_parent -> left = max -> left;
-                            } else if(max -> left == NULL && max -> right != NULL) {
-                                max_parent -> left = max -> right;
-                            }
-                            root -> data = max -> data;
-                            delete max;
-                        }
+                    while(max -> left != NULL) {
+                        max_parent = max;
+                        max = max -> left;
                     }
-                }
-            } else if(parent -> left != NULL && parent -> left -> data == value) {
-                if(parent -> left -> left == NULL && parent -> left -> right == NULL) {
-                    struct node* temp = parent -> left;
-                    parent -> left = NULL;
-                    delete temp;
-                } else if(parent -> left -> left != NULL && parent -> left -> right == NULL) {
-                    struct node* temp = parent -> left;
-                    parent -> left = parent -> left -> left;
-                    delete temp;
-                } else if (parent -> left -> right != NULL && parent -> left -> left == NULL) {
-                    struct node* temp = parent -> left;
-                    parent -> left = parent -> left -> right;
-                    delete temp;
-                } else if (parent -> left -> left != NULL && parent -> left -> right != NULL) {
-                    struct node* max = parent -> left -> right;
-                    struct node* max_parent = parent -> left;
-                    if(max -> left == NULL) {
-                        parent -> left -> data = max -> data;
-                        parent -> left -> right = max -> right;
-                        delete max;
-                    }
-                    else {
-                        while(max -> left != NULL) {
-                            max_parent = max;
-                            max = max -> left;
-                        }
-                        if(max -> left == NULL && max -> right == NULL) {
-                            max_parent -> left = NULL;
-                        } else if(max -> left != NULL && max -> right == NULL) {
-                            max_parent -> left = max -> left;
-                        } else if(max -> left == NULL && max -> right != NULL) {
-                            max_parent -> left = max -> right;
-                        }
-                        parent -> left -> data = max -> data;
-                        delete max;
-                    }
-                }
-            } else if (parent -> right != NULL && parent -> right -> data == value) {
-                if(parent -> right -> left == NULL && parent -> right -> right == NULL) {
-                    struct node* temp = parent -> right;
-                    parent -> right = NULL;
-                    delete temp;
-                } else if(parent -> right -> left != NULL && parent -> right -> right == NULL) {
-                    struct node* temp = parent -> right;
-                    parent -> right = parent -> right -> left;
-                    delete temp;
-                } else if (parent -> right -> right != NULL && parent -> right -> left == NULL) {
-                    struct node* temp = parent -> right;
-                    parent -> right = parent -> right -> right;
-                    delete temp;
-                } else if (parent -> right -> left != NULL && parent -> right -> right != NULL) {
-                    struct node* max = parent -> right -> right;
-                    struct node* max_parent = parent -> right;
-                    if(max -> left == NULL) {
-                        parent -> right -> data = max -> data;
-                        parent -> right -> right = max -> right;
-                        delete max;
-                    }
-                    else {
-                        while(max -> left != NULL) {
-                            max_parent = max;
-                            max = max -> left;
-                        }
-                        if(max -> left == NULL && max -> right == NULL) {
-                            max_parent -> left = NULL;
-                        } else if(max -> left != NULL && max -> right == NULL) {
-                            max_parent -> left = max -> left;
-                        } else if(max -> left == NULL && max -> right != NULL) {
-                            max_parent -> left = max -> right;
-                        }
-                        parent -> right -> data = max -> data;
-                        delete max;
-                    }
+                    if(max -> right == NULL) max_parent -> left = NULL;
+                    else if(max -> right != NULL) max_parent -> left = max -> right;
+                    ptr -> data = max -> data;
+                    delete max;
                 }
             }
         }
@@ -247,10 +180,24 @@ class BST {
 
 main() {
     BST tree;
+    // Initialise/Insertion
     tree.initialise();
+    // Display
     tree.display();
-    // tree.search(12);
-    // tree.search(14);
+    // Traversal
+    tree.preorderTraversal();
+    tree.postorderTraversal();
+    tree.inorderTraversal();
+    // Search
+    tree.search(123);
+    tree.search(14);
+    // Delete
     tree.delete_(9);
+    tree.display();
+    tree.delete_(11);
+    tree.display();
+    tree.delete_(67);
+    tree.display();
+    tree.delete_(23);
     tree.display();
 }
